b0f2145cab81d040ad9a1e05f2c254451df33b22
JaMach <45195890+JaMach@users.noreply.github.com>
2018-11-24 17:07:15 +0100

    Add files via upload

---
+++ Prueba_movimiento/Prueba_movimiento/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Prueba_movimiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(42, 120);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(34, 25);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            //this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            //this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            //this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Prueba_movimiento/Prueba_movimiento/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prueba_movimiento
+{
+    public partial class Form1 : Form
+    {
+        private Point mouseOffset;//Objeto Point nuevo. Esto actuará como variable cuando calcule cómo mover el formulario. 
+        private bool isMouseDown = false;//El campo isMouseDown se utiliza para controlar si el usuario mantiene presionado el botón del mouse.
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }//Boton Salir
+
+        //-------------------------TRES EVENTOS PARA MOVER EL FORM: MouseDown, MouseMove y MouseUp----------------------------------\\
+
+        /*private void Form1_MouseDown(object sender, MouseEventArgs e)//Pulsamos boton del mouse
+        {
+            int xOffset;//Asignar coordenadas a la variable mouseOffset en función de la posición actual del puntero del mouse.
+            int yOffset;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;//la posición de desplazamiento se calcula utilizando información del  
+                yOffset = -e.Y - SystemInformation.CaptionHeight -//sistema sobre el tamaño del borde (FrameBorderSize.Width)
+                    SystemInformation.FrameBorderSize.Height;// y el alto de la barra de título (CaptionHeight).
+                mouseOffset = new Point(xOffset, yOffset);//Asignamos la posicion del desplazamiento a la variable Point.
+                isMouseDown = true;//Está pulsado el boton del mouse.
+            }
+        }*/
+
+        /*private void Form1_MouseMove(object sender, MouseEventArgs e)//Movemos el mouse
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;//Cuando se hace clic con el botón primario del mouse y se arrastra el mouse, 
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;//la propiedad Location del formulario se establece en la nueva posición.
+            }
+        }*/
+
+       /*private void Form1_MouseUp(object sender, MouseEventArgs e)//Levantamos el boton del mouse
+        {
+            if (e.Button == MouseButtons.Left)//El movimiento solo se produce pisando el boton izquierdo del mouse.
+            {
+                isMouseDown = false;
+            }
+
+        }*/
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xOffset;//Asignar coordenadas a la variable mouseOffset en función de la posición actual del puntero del mouse.
+            int yOffset;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;//la posición de desplazamiento se calcula utilizando información del  
+                yOffset = -e.Y - 100 - SystemInformation.CaptionHeight -//sistema sobre el tamaño del borde (FrameBorderSize.Width)
+                    SystemInformation.FrameBorderSize.Height;// y el alto de la barra de título (CaptionHeight).
+                mouseOffset = new Point(xOffset, yOffset);//Asignamos la posicion del desplazamiento a la variable Point.
+                isMouseDown = true;//Está pulsado el boton del mouse.
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;//Cuando se hace clic con el botón primario del mouse y se arrastra el mouse, 
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;//la propiedad Location del formulario se establece en la nueva posición.
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)//El movimiento solo se produce pisando el boton izquierdo del mouse.
+            {
+                isMouseDown = false;
+            }
+        }
+
+        
+
+        //-------------------------\\------------------------------//-----------------------------\\--------------------------------\\
+        //Aqui esta el codigo:
+        //http://msdn.microsoft.com/es-es/library/6k15y9et(VS.80).aspx#Y1257
+        //-------------------------\\------------------------------//-----------------------------\\--------------------------------\\
+
+    }
+}
